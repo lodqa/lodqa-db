5d7c3af22538045417ac9d1cc08f09f278a7ec93
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-11-12 17:03:00 +0900

    Make tests executable

--- test/functional/targets_controller_test.rb
+++ test/functional/targets_controller_test.rb
@@ -1,45 +1,57 @@
 require 'test_helper'
 
 class TargetsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @target = targets(:one)
+    @target.user = users(:one)
+    @target.save!
+    sign_in @target.user
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
-    assert_not_nil assigns(:targets)
+    assert_not_nil assigns(:targets_grid)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create target" do
+  test 'should create target' do
     assert_difference('Target.count') do
-      post :create, target: { description: @target.description, dictionary_url: @target.dictionary_url, endpoint_url: @target.endpoint_url, graph_uri: @target.graph_uri, ignore_predicates: @target.ignore_predicates, max_hop: @target.max_hop, name: @target.name, parser_url: @target.parser_url, sample_queries: @target.sample_queries, sortal_predicates: @target.sortal_predicates }
+      post :create, target: {
+        name: 'tree',
+        endpoint_url: 'http://example.com',
+        dictionary_url: 'http://example.com',
+        sample_queries: 'MyText',
+        sortal_predicates: 'MyText',
+        ignore_predicates: 'MyText',
+      }
     end
 
     assert_redirected_to target_path(assigns(:target))
   end
 
-  test "should show target" do
+  test 'should show target' do
     get :show, id: @target
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @target
     assert_response :success
   end
 
-  test "should update target" do
+  test 'should update target' do
     put :update, id: @target, target: { description: @target.description, dictionary_url: @target.dictionary_url, endpoint_url: @target.endpoint_url, graph_uri: @target.graph_uri, ignore_predicates: @target.ignore_predicates, max_hop: @target.max_hop, name: @target.name, parser_url: @target.parser_url, sample_queries: @target.sample_queries, sortal_predicates: @target.sortal_predicates }
     assert_redirected_to target_path(assigns(:target))
   end
 
-  test "should destroy target" do
+  test 'should destroy target' do
     assert_difference('Target.count', -1) do
       delete :destroy, id: @target
     end
