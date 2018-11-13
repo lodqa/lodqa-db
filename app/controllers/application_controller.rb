aa863109f6aab2405545b678bf98c36783406fd1
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-11-13 16:26:17 +0900

    rails4.0: The Strong Parameters was added. Set the Strong Parameters for Device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
 end
