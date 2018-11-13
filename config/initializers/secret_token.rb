945bb40fc43f01f37d1436e2bf46f1cf6de60671
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-11-13 18:44:04 +0900

    rails4.0: The secret_token changed to the secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-LodqaDb::Application.config.secret_token = '335f8525704de2aa6a8f08c8fb851b5ff765457a29d4aaaf88f064e1bf77ad41a3f168bb964c95b4d7f8d919919d21e64f54bbeaa957991d25e00a78cbf2f3b1'
+LodqaDb::Application.config.secret_key_base = '16564f221ca7cdab968a0f878038790f0840e4b59836fea427bd5f1f3cc79f89af3dc6fe6a4484641bfcac51afdda2c3ae02961ab8f68161196680fc24475330'
