8d9ac9bc00abf65aae5e4f52b9acab6a97288b17
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-11-14 15:31:52 +0900

    rails4.1: Use config/secret.yml

--- config/initializers/secret_token.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-LodqaDb::Application.config.secret_key_base = '16564f221ca7cdab968a0f878038790f0840e4b59836fea427bd5f1f3cc79f89af3dc6fe6a4484641bfcac51afdda2c3ae02961ab8f68161196680fc24475330'
