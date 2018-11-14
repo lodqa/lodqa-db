7d1db27f328b7e0382004ad0a0b545ec1dd671ba
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-11-14 14:56:21 +0900

    bugfix: The link destination of the login user name in the menu is the target list owned by the logged-in user. The narrowing down item is an e-mail address but the value is a user name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ require 'uri'
 
 class UsersController < ApplicationController
   def show
-    redirect_to targets_path + "?grid[f][users.email]=#{params[:username]}"
+    redirect_to targets_path + "?grid[f][users.email]=#{User.find_by!(username: params[:username]).email}"
   end
 end
