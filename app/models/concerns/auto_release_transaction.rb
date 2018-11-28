7053a5211b9172c688e80435ee74e6f790e01d1f
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-11-28 15:53:16 +0900

    feature: Disable SQL log to reduce connection use time

--- app/models/concerns/auto_release_transaction.rb
+++ app/models/concerns/auto_release_transaction.rb
@@ -8,7 +8,14 @@ module AutoReleaseTransaction
     class <<self
       alias_method :orig_transaction, :transaction
       def transaction options = {}, &block
-        orig_transaction(options, &block)
+        # Disable SQL log to reduce connection use time
+        orig_logger = ActiveRecord::Base.logger
+        begin
+          ActiveRecord::Base.logger = nil
+          orig_transaction(options, &block)
+        ensure
+          ActiveRecord::Base.logger = orig_logger
+        end
       ensure
         connection_pool.checkin connection
       end
