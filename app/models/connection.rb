303283eaa106efecae4aedd42f6176655744db94
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2018-12-07 11:50:26 +0900

    feature: Breadth-First Traversal algorithm

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -5,14 +5,80 @@ class Connection < ActiveRecord::Base
   include AcquiredCount
   include CleanGarbage
 
-  def self.append target_name, connections
-    transaction do
-      connections.each do |subject, object|
-        where(target_name: target_name,
-              subject: subject,
-              object: object)
-          .first_or_create
+  class << self
+    def append target_name, connections
+      transaction do
+        connections.each do |subject, object|
+          where(target_name: target_name,
+                subject: subject,
+                object: object)
+            .first_or_create
+        end
       end
     end
+
+    # rubocop:disable Metrics/MethodLength
+    def breadth_first_traversal target_name
+      sql = <<~'SQL'
+        SELECT
+          L.SUBJECT,
+          R.OBJECT
+        FROM CONNECTIONS AS L
+        INNER JOIN CONNECTIONS AS R
+        ON L.TARGET_NAME = R.TARGET_NAME
+          AND L.OBJECT = R.SUBJECT
+        WHERE L.TARGET_NAME = ?
+        UNION
+        SELECT
+          L.SUBJECT,
+          R.SUBJECT
+        FROM CONNECTIONS AS L
+        INNER JOIN CONNECTIONS AS R
+        ON L.TARGET_NAME = R.TARGET_NAME
+          AND L.OBJECT = R.OBJECT
+        WHERE L.TARGET_NAME = ?
+        UNION
+        SELECT
+          L.OBJECT,
+          R.OBJECT
+        FROM CONNECTIONS AS L
+        INNER JOIN CONNECTIONS AS R
+        ON L.TARGET_NAME = R.TARGET_NAME
+          AND L.SUBJECT = R.SUBJECT
+        WHERE L.TARGET_NAME = ?
+        UNION
+        SELECT
+          L.OBJECT,
+          R.SUBJECT
+        FROM CONNECTIONS AS L
+        INNER JOIN CONNECTIONS AS R
+        ON L.TARGET_NAME = R.TARGET_NAME
+          AND L.SUBJECT = R.OBJECT
+        WHERE L.TARGET_NAME = ?
+        EXCEPT
+        SELECT
+          SUBJECT,
+          OBJECT
+        FROM CONNECTIONS
+        WHERE TARGET_NAME = ?
+        EXCEPT
+        SELECT
+          OBJECT,
+          SUBJECT
+        FROM CONNECTIONS
+        WHERE TARGET_NAME = ?
+      SQL
+
+      connection.select_all sanitize_sql_array [
+        sql,
+        target_name,
+        target_name,
+        target_name,
+        target_name,
+        target_name,
+        target_name
+      ]
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
--- test/models/connection_test.rb
+++ test/models/connection_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ConnectionTest < ActiveSupport::TestCase
@@ -5,20 +7,57 @@ class ConnectionTest < ActiveSupport::TestCase
     assert_not_nil Connection.new
   end
 
-  test 'that it able to insert multi record' do
-    Connection.append 'target_one', [
-      ['http://exapmle.com/subject1', 'http://exapmle.com/object1'],
-      ['http://exapmle.com/subject2', 'http://exapmle.com/object2']
-    ]
-    assert_equal 2, Connection.where(target_name: 'target_one').count
+  sub_test_case 'Breadth-First Traversal' do
+    test '1-2, 2-2 is get from 1-2 connection' do
+      result = Connection.breadth_first_traversal 'conncetion_set_one'
+      assert_equal 2, result.length
+      assert result.include?('subject' => '1', 'object' => '1')
+      assert result.include?('subject' => '2', 'object' => '2')
+    end
+
+    test '1-1, 2-2, 3-3, 1-3 and 3-1 are get from 1-2 2-3 connection' do
+      result = Connection.breadth_first_traversal 'conncetion_set_two'
+      assert_equal 5, result.length
+      assert result.include?('subject' => '1', 'object' => '1')
+      assert result.include?('subject' => '2', 'object' => '2')
+      assert result.include?('subject' => '3', 'object' => '3')
+      assert result.include?('subject' => '1', 'object' => '3')
+      assert result.include?('subject' => '3', 'object' => '1')
+    end
+
+    test '1-1 is get from 1-2 2-2 connection' do
+      result = Connection.breadth_first_traversal 'conncetion_set_three'
+      assert_equal 1, result.length
+      assert result.include?('subject' => '1', 'object' => '1')
+    end
+
+    test '1-1, 2-2 ,3-3, 2-3 and 3-2 are get from 1-2 1-3 connection' do
+      result = Connection.breadth_first_traversal 'conncetion_set_four'
+      assert_equal 3, result.length
+      assert result.include?('subject' => '1', 'object' => '1')
+      assert result.include?('subject' => '2', 'object' => '2')
+      assert result.include?('subject' => '3', 'object' => '3')
+      assert result.include?('subject' => '2', 'object' => '3')
+      assert result.include?('subject' => '3', 'object' => '2')
+    end
   end
 
-  test 'that it able to insert multi record without duplication' do
-    Connection.append 'target_one', [
-      ['http://exapmle.com/subject1', 'http://exapmle.com/object1'],
-      ['http://exapmle.com/subject2', 'http://exapmle.com/object2'],
-      ['http://exapmle.com/subject2', 'http://exapmle.com/object2']
-    ]
-    assert_equal 2, Connection.where(target_name: 'target_one').count
+  sub_test_case 'append' do
+    test 'that it able to insert multi record' do
+      Connection.append 'target_one', [
+        ['http://exapmle.com/subject1', 'http://exapmle.com/object1'],
+        ['http://exapmle.com/subject2', 'http://exapmle.com/object2']
+      ]
+      assert_equal 2, Connection.where(target_name: 'target_one').count
+    end
+
+    test 'that it able to insert multi record without duplication' do
+      Connection.append 'target_one', [
+        ['http://exapmle.com/subject1', 'http://exapmle.com/object1'],
+        ['http://exapmle.com/subject2', 'http://exapmle.com/object2'],
+        ['http://exapmle.com/subject2', 'http://exapmle.com/object2']
+      ]
+      assert_equal 2, Connection.where(target_name: 'target_one').count
+    end
   end
 end
