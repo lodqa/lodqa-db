96eccccbc0788c786fb29783a1fa8e3f441d0728
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-10-19 16:06:03 +0900

    misc: Update config for Rails 5.0.7.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-LodqaDb::Application.routes.draw do
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :targets do
     collection do
       get 'names'
