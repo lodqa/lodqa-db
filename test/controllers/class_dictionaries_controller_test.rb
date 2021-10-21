cbda83d888ead61e07c8c6395d2805287bbd2c6a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2021-10-21 11:18:35 +0900

    refactor: Rewrite controller unit tests into feature tests

--- test/controllers/class_dictionaries_controller_test.rb
+++ test/controllers/class_dictionaries_controller_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 # Since the implementation of InstanceDictionariesController and PredicateDictionariesController is equal to ClassDictionariesController,
 # we will substitute this test for their tests.
-class ClassDictionariesControllerTest < ActionController::TestCase
-  include Devise::Test::ControllerHelpers
+class ClassDictionariesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 
   sub_test_case 'not logged in' do
     test 'Users who are not logged in can not get a dictionary' do
-      get :show, params: { target_id: targets(:one).name, format: :csv }
+      get target_class_dictionary_url(targets(:one)), params: { format: :csv }
       assert_response :forbidden
     end
   end
@@ -20,18 +20,18 @@ class ClassDictionariesControllerTest < ActionController::TestCase
     end
 
     test 'get the dictionary' do
-      response = get :show, params: { target_id: targets(:one).name, format: :csv }
+      get target_class_dictionary_url(targets(:one)), params: { format: :csv }
       assert_response :success
-      assert_equal ['klass', 'http://example.com/klass'], response.body.split("\t")
+      assert_equal ['klass', 'http://example.com/klass'], @response.body.split("\t")
     end
 
     test 'can not get the dictionary of targets other than yourself' do
-      get :show, params: { target_id: targets(:two).name, format: :csv }
+      get target_class_dictionary_url(targets(:two)), params: { format: :csv }
       assert_response :forbidden
     end
 
     test 'can not get the dictionary of targets that do not exist' do
-      assert_raises(ActiveRecord::RecordNotFound) { get :show, params: { target_id: 'aaaa', format: :csv } }
+      assert_raises(ActiveRecord::RecordNotFound) { get '/targets/aaa/class_dictionary', params: { format: :csv } }
     end
   end
 end
